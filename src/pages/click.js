baca270860bcb87d375d5b96c1d0246b584dbfdb
Matt Trice <matt.trice@gmail.com>
2018-11-27 13:55:50 -0500

    Project on page

--- src/pages/click.js
+++ src/pages/click.js
@@ -1,7 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Parallax, ParallaxLayer } from 'react-spring/dist/addons'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Parallax, ParallaxLayer } from 'react-spring/dist/addons';
+import SVG from '../components/SVG';
+import '../styles/global';
+import styled from 'react-emotion';
+import ProjectCardText from '../components/ProjectCardText';
 
+import { rotate, UpBubblesOne, UpBubblesOneTop, UpBubblesTwo, UpBubblesTwoTop, UpBubblesThree, UpBubblesThreeTop, UpDown, UpDownWide, waveAnimation } from '../styles/animations'
+import topazshottt from '../images/shottt-topaz.png';
+import '../assets/css/krabby.scss';
 // Little helpers ...
 const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 const Pink = ({ children }) => <span style={{ color: '#FF6AC1' }}>{children}</span>
@@ -11,10 +18,41 @@ const Green = ({ children }) => <span style={{ color: '#57EE89' }}>{children}</s
 const Blue = ({ children }) => <span style={{ color: '#57C7FF' }}>{children}</span>
 const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>
 
+const ProjectsWrapper = styled.div`
+  ${tw('flex flex-wrap justify-between mt-8')};
+  display: grid;
+  grid-gap: 4rem;
+  grid-template-columns: 1fr 50%;
+  @media (max-width: 1200px) {
+    grid-gap: 3rem;
+  }
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+    grid-gap: 2rem;
+  }
+`;
+
 class App extends React.Component {
   render() {
     return (
       <Parallax ref={ref => (this.parallax = ref)} pages={3}>
+        <ParallaxLayer offset={0} speed={1}>
+          <UpBubblesOneTop>
+            <SVG icon="bubble" width={3} left="36%" top="32%" />
+            <SVG icon="bubble" width={2} left="40%" top="34%" />
+            <SVG icon="bubble" width={4} left="42%" top="33%" />
+          </UpBubblesOneTop>
+          <UpBubblesTwoTop>
+            <SVG icon="bubble" width={4} left="34%" top="32%" />
+            <SVG icon="bubble" width={3} left="41%" top="34%" />
+            <SVG icon="bubble" width={2} left="39%" top="31%" />
+          </UpBubblesTwoTop>
+          <UpBubblesThreeTop>
+            <SVG icon="bubble" width={2} left="32%" top="36%" />
+            <SVG icon="bubble" width={4} left="38%" top="30%" />
+            <SVG icon="bubble" width={3} left="31%" top="35%" />
+          </UpBubblesThreeTop>
+        </ParallaxLayer>
         <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
         <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
 
@@ -22,6 +60,17 @@ class App extends React.Component {
 
         <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
           <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+          <ProjectsWrapper>
+            <ProjectCardText
+              title="UX DESIGN, FRONT END DEVELOPMENT"
+              link="topaz"
+            >
+              This project was a redesign and rebuild of their attendee check in app. As part of their free ticketing software, this app allows an event organizer to check in participants and provides the organizer with attendance data.
+            </ProjectCardText>
+            <div>
+              <img src={topazshottt} className="shottt-topaz" />
+            </div>
+          </ProjectsWrapper>
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
