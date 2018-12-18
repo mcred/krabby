d0b236baa70bcca681255080b2a9947041b17ac0
Matt Trice <matt.trice@gmail.com>
2018-12-18 12:00:05 -0500

    add test form

--- src/pages/index.js
+++ src/pages/index.js
@@ -311,6 +311,25 @@ class App extends React.Component {
         <ParallaxLayer offset={5} speed={-0}>
           <Container>
             <Hero>
+            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <input type="hidden" name="bot-field" />
+                    <div className="field half first">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="6"></textarea>
+                    </div>
+                    <ul className="actions">
+                        <li><input type="submit" value="Send Message" className="special" /></li>
+                        <li><input type="reset" value="Clear" /></li>
+                    </ul>
+                </form>
               <FormTitle className="item--contained">Let's have a conversation</FormTitle>
               <form class="contact__form item--contained"  name="contact" method="post" action="/herewego/" data-netlify="true" data-netlify-honeypot="bot-field">
                 <input type="hidden" name="bot-field" />
