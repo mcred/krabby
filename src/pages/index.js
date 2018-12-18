44814fa84f3abc450f1642a93d7ba68237752499
Matt Trice <matt.trice@gmail.com>
2018-12-18 12:24:25 -0500

    testing links

--- src/pages/index.js
+++ src/pages/index.js
@@ -311,27 +311,9 @@ class App extends React.Component {
         <ParallaxLayer offset={5} speed={-0}>
           <Container>
             <Hero>
-            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input type="hidden" name="bot-field" />
-                    <div className="field half first">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" />
-                    </div>
-                    <div className="field half">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
-                    </div>
-                    <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
-                    </ul>
-                </form>
+
               <FormTitle className="item--contained">Let's have a conversation</FormTitle>
-              <form class="contact__form item--contained"  name="contact" method="post" action="/herewego/" data-netlify="true" data-netlify-honeypot="bot-field">
+              <form class="contact__form item--contained"  name="contact" method="post" action="herewego" data-netlify="true" data-netlify-honeypot="bot-field">
                 <input type="hidden" name="bot-field" />
                 <FloatingLabel id='name' name='name' placeholder='Name' type='text' styles={inputStyle} required />
                 <FloatingLabel id='email' name='email' placeholder='Email' type='email' styles={inputStyle} required />
@@ -348,6 +330,7 @@ class App extends React.Component {
               <ul className="nav__main">
                 <li>
                   <Link to="/" className=""><img src={logotd} className="logo__main" /></Link>
+                  <Link to="herewego" className=""><img src={logotd} className="logo__main" /></Link>
                 </li>
                 <li>
                   <ul className="social-icons">
